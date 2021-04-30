551ea4e86acb3db4394a68185bd5e8865e90dbd4
je-pa <jieun0502@gmail.com>
2021-04-30 16:41:41 +0900

    2021.04.30_DB연동 list

--- BoardVer3/src/main/java/com/koreait/board3/BoardListServlet3.java
+++ BoardVer3/src/main/java/com/koreait/board3/BoardListServlet3.java
@@ -1,6 +1,8 @@
 package com.koreait.board3;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +14,10 @@ public class BoardListServlet3 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
  
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<BoardVO3> list= BoardDAO.selBoardList(); 
+		//파라미터가 없다는 것은 다 델고 오겠다 범위를 지정하지 않겠다.
+		request.setAttribute("list",list);
+		
 		request.getRequestDispatcher("/WEB-INF/view/list3.jsp").forward(request, response);
 	}
 
