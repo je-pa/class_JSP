ec26ffc3e83a4bfee275d1f2f5fb9dc3b70a0cde
je-pa <jieun0502@gmail.com>
2021-05-12 16:34:58 +0900

    2021.05.12_게시판

--- BoardVer4/src/main/java/com/koreait/board4/MyUtils.java
+++ BoardVer4/src/main/java/com/koreait/board4/MyUtils.java
@@ -5,8 +5,20 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.koreait.board4.user.UserVO;
 
 public class MyUtils {
+	public static UserVO getLoginUser(HttpServletRequest req) {
+		if(req==null) {	return null;}
+		return (UserVO) req.getSession().getAttribute("loginUser");
+	}
+	
+	public static int getLoginUserPK(HttpServletRequest req) {
+		return (getLoginUser(req)).getIuser();
+	}
+	
 	public static void openJSP(String fileNm, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.getRequestDispatcher("/WEB-INF/view/"+fileNm+".jsp").forward(request, response);
 	}
--- BoardVer4/src/main/java/com/koreait/board4/board/BoardDAO.java
+++ BoardVer4/src/main/java/com/koreait/board4/board/BoardDAO.java
@@ -35,7 +35,7 @@ public class BoardDAO {
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 		String sql ="SELECT a.iboard, a.title, a.regdt, b.unm FROM t_board a "
-				+ " LEFT JOIN t_user b ON a.iuser=b.iuser";
+				+ " LEFT JOIN t_user b ON a.iuser=b.iuser ORDER BY a.iboard DESC";
 		try {
 			con=DBUtils.getCon();
 			ps = con.prepareStatement(sql);
@@ -56,5 +56,71 @@ public class BoardDAO {
 		}
 		
 		return list;
+	}
+	
+	public static BoardVO selBoard(int iboard){
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		BoardVO vo = null;
+		String sql ="SELECT a.title, a.ctnt, a.regdt,b.iuser, b.unm FROM t_board a "
+				+ " LEFT JOIN t_user b ON a.iuser=b.iuser WHERE a.iboard=?";
+		try {
+			con=DBUtils.getCon();
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, iboard);
+			rs=ps.executeQuery();
+			if(rs.next()) {
+				vo = new BoardVO();
+				vo.setIboard(iboard);
+				vo.setTitle(rs.getString("title"));
+				vo.setCtnt(rs.getString("ctnt"));
+				vo.setRegdt(rs.getString("regdt"));
+				vo.setIuser(rs.getInt("iuser"));
+				vo.setUnm(rs.getString("unm"));
+			}
+			return vo;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return vo;
+		} finally {
+			DBUtils.close(con, ps, rs);
+		}
+	}
+	public static int updBoard(BoardVO vo) {
+		Connection con = null;
+		PreparedStatement ps = null;
+		String sql = "UPDATE t_board SET title=?,ctnt=? WHERE iboard =?";
+		try {
+			con=DBUtils.getCon();
+			ps=con.prepareStatement(sql);
+			ps.setString(1, vo.getTitle());
+			ps.setString(2, vo.getCtnt());
+			ps.setInt(3, vo.getIboard());
+			
+			return ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBUtils.close(con, ps);
+		}
+		return 0;
+	}
+	public static int delBoard(BoardVO vo) {
+		Connection con = null;
+		PreparedStatement ps = null;
+		String sql = "DELETE FROM t_board WHERE iboard =?";
+		try {
+			con=DBUtils.getCon();
+			ps=con.prepareStatement(sql);
+			ps.setInt(1, vo.getIboard());
+			
+			return ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBUtils.close(con, ps);
+		}
+		return 0;
 	}
 }
