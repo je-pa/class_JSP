673bfc998e3609714ab8d6d8aaf6f7d67b40df70
je-pa <jieun0502@gmail.com>
2021-05-13 17:02:13 +0900

    2021.05.13_연습

---
+++ Prac2/src/main/java/com/koreait/prac2/MyUtils.java
@@ -0,0 +1,28 @@
+package com.koreait.prac2;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.koreait.prac2.user.UserVO;
+
+public class MyUtils {
+	public static void openJSP(String jsp ,HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		req.getRequestDispatcher("/WEB-INF/view/"+jsp+".jsp").forward(req, res);
+	}
+	public static int stringToInt(String str) {
+		return Integer.parseInt(str);
+	}
+	public static int getIntParam(String str , HttpServletRequest req) {
+		return stringToInt(req.getParameter(str));
+	}
+	public static UserVO getLoginUser(HttpServletRequest req) {
+		if(req==null) {	return null;}
+		return (UserVO)req.getSession().getAttribute("loginUser");
+	}
+	public static int getLoginUserPK(HttpServletRequest req) {
+		return getLoginUser(req).getIuser();
+	}
+}
