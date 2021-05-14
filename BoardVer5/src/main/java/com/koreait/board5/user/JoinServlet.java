65acbc3034bc1f6eb369ef8ccf3191b40f9375e3
je-pa <jieun0502@gmail.com>
2021-05-14 16:55:17 +0900

    2021.05.14_게시판_user

---
+++ BoardVer5/src/main/java/com/koreait/board5/user/JoinServlet.java
@@ -0,0 +1,32 @@
+package com.koreait.board5.user;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+import com.koreait.board5.MyUtils;
+
+@WebServlet("/user/join")
+public class JoinServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		MyUtils.openJSP("user/join", request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		UserVO vo = new UserVO();
+		vo.setUid(request.getParameter("uid"));
+		vo.setUpw(BCrypt.hashpw(request.getParameter("upw"), BCrypt.gensalt()) );
+		vo.setUnm(request.getParameter("unm"));
+		vo.setGender(MyUtils.getParamInt("gender",request));
+		UserDAO.joinUser(vo);
+		response.sendRedirect("login");
+	}
+
+}
