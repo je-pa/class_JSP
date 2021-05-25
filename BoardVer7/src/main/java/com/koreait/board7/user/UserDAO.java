3ddf06a69c5ce53f964fcf00506e82198b67551a
je-pa <jieun0502@gmail.com>
2021-05-25 09:10:17 +0900

    2021.05.24_board7_user

---
+++ BoardVer7/src/main/java/com/koreait/board7/user/UserDAO.java
@@ -0,0 +1,59 @@
+package com.koreait.board7.user;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.koreait.board7.DBUtils;
+
+public class UserDAO {
+	//아이디가 있으면 1 없으면 0
+	public static int selIdChk(String uid) {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs =null;
+		String sql = "SELECT iuser FROM t_user WHERE uid=?";
+		try {
+			con = DBUtils.getCon();
+			ps=con.prepareStatement(sql);
+			ps.setString(1, uid.trim());
+			rs=ps.executeQuery();
+			
+			if(rs.next()) {
+				return 1;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBUtils.close(con, ps, rs);
+		}
+		return 0;
+	}
+	public static UserEntity selUser(UserEntity param) {
+		Connection con =null;
+		PreparedStatement ps =null;
+		ResultSet rs = null;
+		
+		UserEntity result = null;
+		String sql = "SELECT iuser, uid, upw, unm FROM t_user WHERE uid=?";
+		try {
+			con= DBUtils.getCon();
+			ps=con.prepareStatement(sql);
+			ps.setString(1, param.getUid());
+			rs=ps.executeQuery();
+			if(rs.next()) {
+				result=new UserEntity();
+				result.setIuser(rs.getInt("iuser"));
+				result.setUid(rs.getString("uid"));
+				result.setUpw(rs.getString("upw"));
+				result.setUnm(rs.getString("unm"));
+			}
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return result;
+		} finally {
+			DBUtils.close(con, ps, rs);
+		}
+	}
+}
---
+++ BoardVer7/src/main/webapp/WEB-INF/view/user/userLogin.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<h1>login</h1>
+<div>
+	<div class="errMsg">${errMsg }</div>
+	<form action="login" method="post">
+			<div><input type ="text" name="uid" placeholder="아이디"></div>
+			<div><input type ="password" name="upw" placeholder="비밀번호"></div>			
+			<div><input type="submit" value="Login"></div>
+	</form>
+</div>
+<div>
+	<a href="join">회원가입</a>
+</div>
